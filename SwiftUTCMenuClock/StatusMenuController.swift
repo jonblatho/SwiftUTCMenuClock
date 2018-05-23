e0f119e150c3bb6c4c5e8a7dacd788731a775c2f
Jonathan Thornton <jonblatho@icloud.com>
2018-05-22 23:54:04 -0500

    Added a check to see if the time string really needs to be updated before updating it. Energy usage decreased—a lot. Closes #1.

--- SwiftUTCMenuClock/StatusMenuController.swift
+++ SwiftUTCMenuClock/StatusMenuController.swift
@@ -23,23 +23,40 @@ class StatusMenuController: NSObject {
 		formatter.timeZone = TimeZone(identifier: "Etc/UTC")
 		formatter.dateFormat = "HH:mm zzz"
 		
-		// Set up a timer running every 0.03 seconds that gets the current time and formats it for the menu bar.
+		let calendar = Calendar.current
+		
+		// Set up a timer running every 0.01 seconds that gets the current time and formats it, if needed, for the menu bar.
 		// (Most other utilities like this one run a timer like this every 1.00 seconds, which is bad and wrong.)
-		Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { _ in
+		Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
 			let now = Date(timeIntervalSinceNow: 0)
 			
-			let dateString = formatter.string(from: now)
+			// Check the current millisecond to see if we actually need to update the time string.
+			let millisecond = calendar.component(.nanosecond, from: now) / 1000000
+			switch millisecond {
+			case 0...9:
+				// We've got a second on the second (approximately). We need to update the time.
+				// Format the current time into a human-readable string and set the title.
+				let timeString = formatter.string(from: now)
+				self.statusItem.attributedTitle = self.formatTimeString(string: timeString)
+			default:
+				// We don't need to format the time, so...do nothing.
+				break
+			}
 			
-			// To style this with monospaced digits (so other menu bar items don't wobble side-to-side with variable digit widths), we need to set up a NSMutableAttributedString. Joy!
-			let attributedString = NSMutableAttributedString(string: dateString)
-			// Set the range to cover the entire character range.
-			let range = NSRange(location: 0, length: dateString.count)
-			// And finally, apply the monospaced digit system font.
-			attributedString.addAttribute(.font, value: NSFont.monospacedDigitSystemFont(ofSize: 14, weight: .regular), range: range)
 			
-			// And it's done! Set the title for the menu bar item.
-			self.statusItem.attributedTitle = attributedString
 		}
+	}
+	
+	private func formatTimeString(string: String) -> NSMutableAttributedString {
+		// Set up an attributed string.
+		let attributedString = NSMutableAttributedString(string: string)
+		// Set the range to cover the entire character range.
+		let range = NSRange(location: 0, length: string.count)
+		// Apply the monospaced digit system font.
+		attributedString.addAttribute(.font, value: NSFont.monospacedDigitSystemFont(ofSize: 14, weight: .regular), range: range)
+		
+		// We're done here; return the attributed string.
+		return attributedString
 	}
 	
 	@IBAction func quitClicked(_ sender: Any) {
